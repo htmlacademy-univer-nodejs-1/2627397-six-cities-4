686ece093527f4f9d7ecd48f0a576d9744a305fb
dengoan969 <dengoan969@gmail.com>
2025-05-12 21:54:32 +0500

    Добавить модель для комментариев

---
+++ src/shared/modules/comment/default-comment.service.ts
@@ -0,0 +1,31 @@
+import { injectable, inject } from 'inversify';
+import { CommentModel } from './comment.entity.js';
+import { OfferService } from '../offer/offer-service.interface.js';
+import { Logger } from '../../libs/logger/logger.interface.js';
+import { Component } from '../../types/component.enum.js';
+
+@injectable()
+export class DefaultCommentService {
+  constructor(
+    @inject(Component.Logger) private logger: Logger,
+    @inject(Component.CommentModel) private model: typeof CommentModel,
+    @inject(Component.OfferService) private offerService: OfferService
+  ) {}
+
+  public async createComment(dto: { text: string, rating: number, offerId: string, authorId: string }) {
+    const comment = new this.model({
+      text: dto.text,
+      postDate: new Date(),
+      rating: dto.rating,
+      offerId: dto.offerId,
+      author: dto.authorId
+});
+
+    await comment.save();
+
+    await this.offerService.incCommentCount(dto.offerId);
+
+    this.logger.info(`Created comment for offer ${dto.offerId}`);
+    return comment;
+  }
+}
--- src/shared/modules/offer/default-offer.service.ts
+++ src/shared/modules/offer/default-offer.service.ts
@@ -3,12 +3,14 @@ import { OfferService } from './offer-service.interface.js';
 import { OfferModel, OfferEntity } from './offer.entity.js';
 import { Logger } from '../../libs/logger/logger.interface.js';
 import { Component } from '../../types/component.enum.js';
+import { CommentModel } from '../comment/comment.entity.js';
 
 @injectable()
 export class DefaultOfferService implements OfferService {
   constructor(
     @inject(Component.Logger) private logger: Logger,
-    @inject(Component.OfferModel) private model: typeof OfferModel
+    @inject(Component.OfferModel) private model: typeof OfferModel,
+    @inject(Component.CommentModel) private commentModel: typeof CommentModel
   ) {}
 
   public async create(dto: Partial<OfferEntity>) {
@@ -20,5 +22,33 @@ export class DefaultOfferService implements OfferService {
 
   public async findById(id: string) {
     return this.model.findById(id).exec();
+  }
+
+  public async updateRating(offerId: string) {
+    const offer = await this.model.findById(offerId).exec();
+    if (!offer) {
+      throw new Error('Offer not found');
+    }
+
+    const reviews = await this.commentModel.find({ offerId });
+    const rating = reviews.reduce((sum, review) => sum + review.rating, 0) / reviews.length;
+    offer.rating = rating;
+
+    await offer.save();
+    this.logger.info(`New rating for offer ${offerId}: ${rating}`);
+  }
+
+  public async incCommentCount(offerId: string) {
+    const offer = await this.model.findById(offerId).exec();
+    if (!offer) {
+      throw new Error('Offer not found');
+    }
+
+    offer.commentCount += 1;
+
+    await offer.save();
+
+    this.logger.info(`Inc comment count for offer ${offerId}. New count: ${offer.commentCount}`);
+    return offer;
   }
 }
